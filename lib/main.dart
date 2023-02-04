503c3a91dbf9716b42e0edc9d1a941490e055cb1
Nattapon Suwanno <tape@Nattapons-MacBook-Pro.local>
2023-02-04 13:46:09 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Text("SWU"),
+    );
+  }
+}
