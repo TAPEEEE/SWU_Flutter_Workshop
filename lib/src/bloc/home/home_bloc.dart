5bd9ad306bb946583e39c964588fbf3d7b705b45
Nattapon Suwanno <nattapon.tape@g.swu.ac.th>
2023-02-12 16:59:55 +0700

    Day 2

---
+++ lib/src/bloc/home/home_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:demo0/src/models/youtube_response.dart';
+import 'package:dio/dio.dart';
+import 'package:equatable/equatable.dart';
+
+part 'home_event.dart';
+part 'home_state.dart';
+
+class HomeBloc extends Bloc<HomeEvent, HomeState> {
+  HomeBloc() : super(HomeState()) {
+    on<HomeEventLoadData>((event, emit) async {
+      final dio = Dio();
+      final url = "https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=songs";
+      final response  = await dio.get(url);
+      final youtubeResponse  = youtubeResponseFromJson(response.data);
+      emit(state.copyWith(youtubes: youtubeResponse.youtubes));
+    });
+  }
+}
